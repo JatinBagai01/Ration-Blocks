8f6fc8ae0aac43b7ee40639f2b21ec0b83e116d9
JatinBagai <jatin.bagai01@gmail.com>
2021-11-11 22:12:04 +0530

    New

---
+++ header.php
@@ -0,0 +1,144 @@
+    <header class="main-header header-style-two">
+
+        <!-- Header Top -->
+        <div class="header-top">
+            <div class="auto-container clearfix">
+                <!--Top Left-->
+                <!-- <div class="top-left pull-left">
+                    <ul class="links-nav clearfix">
+                        <li><span class="icon fa fa-envelope-o"></span><a href="#">contact@rationblocks@helpdesk.gov.in</a></li>
+                    </ul>
+                </div> -->
+
+                <!-- <div class="top-left pull-left"> -->
+
+                <!-- <ul class="center"> -->
+                <!-- <a href="#"><h2>Ration Blocks</h2></a> -->
+                <!-- </ul> -->
+                <!-- </div> -->
+
+                <!--Top Right-->
+                <!-- <div class="top-right pull-right">
+                    <ul class="links-nav clearfix">
+                        <li><span class="icon fa fa-phone"></span><a href="#">Call Us Now : +91-9810256713</a></li>
+                    </ul>
+
+                </div> -->
+                <div class="center" style="text-align: center;">
+                    <a href="#">
+                        <h2>Ration Blocks</h2>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+        <!-- Header Top End -->
+
+        <!--Header-Upper-->
+        <div class="header-upper">
+            <div class="auto-container">
+                <div class="clearfix">
+                    <!--Logo Outer-->
+
+
+
+                    <div class="logo-outer" style="width: 500px;">
+                        <div class="logo"><a href="index.php"><img src="images\emofpi.png" alt="" title=""></a></div>
+                    </div>
+                    <div class="logo-outer">
+                        <div class="logo"><a href="index.php"><img src="images\mofpi.png" alt="" title=""></a></div>
+                    </div>
+                    <div class="logo-outer">
+                        <div class="logo"><a href="index.php"><img src="images\swachbharat.png" alt="" title=""></a></div>
+                    </div>
+                    <div class="logo-outer">
+                        <div class="logo" style="width: 120px; "><a href="index.php"><img src="images\modiji.png" alt="" title=""></a></div>
+                    </div>
+                    <!-- <div class="logo-outer">
+                        <div class="logo"><a href="index.php"><img src="images\toll.png" alt="" title=""></a></div>
+                    </div> -->
+                    <!-- <div class="logo-outer">
+                        <div class="logo"><a href="index.php"><img src="images\mofpiTEXT.png" alt="" title=""></a></div>
+                    </div> -->
+
+
+                    <!-- <div class="nav-outer clearfix"> -->
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <!-- <div class="navbar-header"> -->
+                        <!-- Toggle Button -->
+                        <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div> -->
+
+
+                        <!-- <div class="navbar-collapse collapse clearfix"> -->
+                        <!-- <div class="header-top"> -->
+                            <div class="auto-container clearfix">
+                                <!--Left Nav-->
+                                <ul class="navigation clearfix">
+
+                                    <!-- <li><a href="result.php">Result</a></li> -->
+                                    <li><a href="teachers.php">About Us</a></li>
+                                    <li><a href="principle.php">Documents</a></li>
+                                    <li><a href="contact.php">Login</a></li>
+                                    <li><a href="#">Register</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                    <li><a href="#">Home</a></li>
+                                </ul>
+
+                            <!-- </div> -->
+                        </div>
+                        <!-- </div> -->
+                    </nav>
+                    <!-- </div> -->
+
+                </div>
+            </div>
+        </div>
+        <!--End Header Upper-->
+
+        <!--Sticky Header-->
+        <div class="sticky-header">
+            <div class="auto-container clearfix">
+                <!--Logo-->
+                <div class="logo pull-left">
+                    <a href="index.php" class="img-responsive"><img src="images\ration-logo.png" style="width:60px;" alt="" title=""></a>
+                </div>
+
+                <!--Right Col-->
+                <div class="right-col pull-right">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+
+                                <!-- <li><a href="result.php">Result</a></li> -->
+                                <li><a href="teachers.php">About Us</a></li>
+                                <li><a href="principle.php">Documents</a></li>
+                                <li><a href="contact.php">Login</a></li>
+                                <li><a href="#">Register</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Home</a></li>
+                            </ul>
+                        </div>
+                    </nav><!-- Main Menu End-->
+                </div>
+
+            </div>
+        </div>
+        <!--End Sticky Header-->
+
+    </header>
